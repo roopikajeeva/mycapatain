8dbe803e8dcb8f8b35619735e7f5127103f96a6a
roopikajeeva <56499402+roopikajeeva@users.noreply.github.com>
2019-10-13 12:32:35 +0530

    Add files via upload

---
+++ task2temperature.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float celcius,farenheit;
+   printf("Enter the temperature in celcius:\n");
+   scanf("%f",&celcius);
+
+   farenheit=(celcius*9/5)+32;
+
+   printf("The given temperature in farenheit is %f",farenheit);
+
+
+
+
+
+}
